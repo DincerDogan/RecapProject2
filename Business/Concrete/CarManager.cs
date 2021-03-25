a2546254e5d28544681376b20fb0599d199cad25
DINCER D <dincerdogan@gmail.com>
2021-03-25 17:43:46 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,116 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using Entity.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Business.Constants;
+using Core.Utilities.Results;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public IResult AddCar(Car car)
+        {
+            if (car.DailyPrice > 0 && car.Description.Length > 2)
+            {
+                _carDal.Add(car);
+                return new SuccessResult(Messages.CarAdded);
+            }
+            else if (car.DailyPrice <= 0)
+                return new ErrorResult(Messages.CarDailyPriceInvalid);
+            else if (car.Description.Length <= 2)
+                return new ErrorResult(Messages.CarDescriptionInvalid);
+            else return new ErrorResult(Messages.InvalidRequest);
+
+        }
+
+        public IDataResult<List<CarDetailDto>> GetCarDetailsByColorId(int colorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarDetailDto>> GetCarDetailsByBrandId(int brandId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Car>> GetAllCarsIsNotNull()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult DeleteCar(Car car)
+        {
+            _carDal.Delete(car);
+            return new SuccessResult(Messages.CarDeleted);
+        }
+
+        public IDataResult<List<Car>> GetAllCar()
+        {
+         // return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.Description.Length > 3),Messages.ProductAdded);//Adi 3 karakterten buyuk olan extra
+
+         if (DateTime.Now.Hour==22)
+            {
+                return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
+            }
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll());
+        }
+
+        public IDataResult<Car> GetById(int carId)
+        {
+            return  new SuccessDataResult<Car>(_carDal.GetById(c => c.Id == carId));
+        }
+
+        public IDataResult<List<CarDetailDto>> GetCarDetails()
+        {
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails());
+        }
+
+        public IDataResult<List<Car>> GetCarsByBrandId(int brandId)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.BrandId == brandId));
+        }
+
+        public IDataResult<List<Car>> GetCarsByColorId(int colorId)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ColorId == colorId));
+        }
+
+        public IDataResult<List<Car>> GetCarsByDailyPrice(decimal min, decimal max)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.DailyPrice >= min && c.DailyPrice <= max));
+        }
+
+        //public List<Car> GetCarById(int id)
+        //{
+        //    return _carDal.GetById( id);
+        //}
+
+        public IResult UpdateCar(Car car)
+        {
+            if (car.DailyPrice > 0 && car.Description.Length > 2)
+            {
+                _carDal.Add(car);
+                return new SuccessResult(Messages.CarUpdated);
+            }
+            else if (car.DailyPrice <= 0)
+                return new ErrorResult(Messages.CarDailyPriceInvalid);
+            else if (car.Description.Length <= 2)
+                return new ErrorResult(Messages.CarDescriptionInvalid);
+            else return new ErrorResult(Messages.InvalidRequest);
+        }
+
+
+    }
+}
---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entity.Concrete;
+
+namespace Business.Concrete
+{
+  public  class UserManager : IUserService
+    {
+        IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public IResult Delete(User user)
+        {
+            _userDal.Delete(user);
+            return new SuccessResult(Messages.UserDeleted);
+        }
+
+        public IResult Add(User user)
+        {
+            _userDal.Add(user);
+            return new SuccessResult(Messages.UserAdded);
+        }
+
+        public IResult Update(User user)
+        {
+            _userDal.Update(user);
+            return new SuccessResult(Messages.UserUpdated);
+        }
+
+
+        public IDataResult<List<User>> GetAll()
+        {
+            return new SuccessDataResult<List<User>>(_userDal.GetAll());
+        }
+
+        public IDataResult<User> GetByUserId(int id)
+        {
+            return new SuccessDataResult<User>(_userDal.GetById(u => u.Id == id));
+        }
+    }
+}
+
